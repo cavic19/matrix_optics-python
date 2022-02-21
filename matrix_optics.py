31e6f0823a06515b83e75c8cbe117ca47e9ed285
David <david.tomecek1@seznam.cz>
2022-02-21 13:12:37 +0100

    Init commit

---
+++ matrix_optics.py
@@ -0,0 +1,72 @@
+from multiprocessing.sharedctypes import Value
+import numpy as np
+from functools import reduce
+
+
+class ABCDElement:
+    def __init__(self, *args) -> None:
+        if len(args) == 4:
+            self._A = args[0]
+            self._B = args[1]
+            self._C = args[2]
+            self._D = args[3]
+        elif len(args) == 1 and isinstance(args[0], np.ndarray) and self.__is_square_matrix_of_dim(args[0], 2):
+            self._A = args[0][0][0]
+            self._B = args[0][0][1]
+            self._C = args[0][1][0]
+            self._D = args[0][1][1]
+        else:
+            raise ValueError("No matrix definition present in init.")
+
+    def __is_square_matrix_of_dim(self, m: np.ndarray, dim: int):
+        return all(len(row) == len(m) for row in m) and len(m) == dim
+
+    @property
+    def matrix(self) -> np.ndarray:
+        return np.array([[self._A, self._B], [self._C, self._D]])
+    
+    def act(self, q_param: complex) -> complex:
+        nom = self._A * q_param + self._B
+        denom = self._C * q_param + self._D
+        return nom / denom
+
+
+class FreeSpace(ABCDElement):
+    def __init__(self, d: float) -> None:
+        self.d = d
+        super().__init__(1, d, 0, 1)
+  
+
+class ThinLens(ABCDElement):
+    def __init__(self, f: float) -> None:
+        self.f = f
+        super().__init__(1, 0, -1/f, 1)
+
+
+class OpticalPath:
+    """Represents optical path that is created in init function."""
+    elements: list[ABCDElement] = []
+    def __init__(self, *elements: list[ABCDElement]) -> None:
+        for el in elements:
+            if isinstance(el, (int, float)):
+                self.elements.append(FreeSpace(el))
+            else:
+                self.elements.append(el)
+
+    def propagate(self, q_in: complex) -> complex:
+        system = self.__build_system()
+        return system.act(q_in)
+
+
+    def __build_system(self) -> ABCDElement:
+        system_matrix = reduce(lambda c, b: c.dot(b), [e.matrix for e in reversed(self.elements)])
+        return ABCDElement(system_matrix)
+
+
+
+
+
+if __name__ == "__main__":
+    list = [1,1,1]
+    t = reduce(lambda x, y: x + y, list)
+    print(str(t))
---
+++ test_matrix_optics.py
@@ -0,0 +1,85 @@
+import unittest
+import cmath
+from cv2 import exp
+import numpy as np
+from matrix_optics import ABCDElement, FreeSpace, OpticalPath
+
+class TestABCDElement(unittest.TestCase):
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+    def test_init_matrix_elements_should_equal(s):
+        el = ABCDElement(s.A, s.B, s.C, s.D)
+        expected = np.array([[s.A, s.B], [s.C, s.D]])
+        actual = el.matrix
+        s.assertTrue((expected == actual).all())
+    
+    def test_init_matrix_should_equal(s):
+        expected = np.array([[s.A, s.B], [s.C, s.D]])
+        el = ABCDElement(expected)
+        actual = el.matrix
+        s.assertTrue((expected == actual).all())
+
+
+    def test_init_invalid_args_should_raise(s):
+        with s.assertRaises(ValueError):
+            el = ABCDElement(1, 2, 3, 4, 5)
+        with s.assertRaises(ValueError):
+            el = ABCDElement(np.array([[1,2,3],[1,2,3]]))
+        with s.assertRaises(ValueError):
+            el = ABCDElement(np.array([[1,2],[1]]))
+
+class TestFreeSpace(unittest.TestCase):
+    def test_should_equal(self):
+        q_in = complex(3,4)
+        free_space = FreeSpace(10)
+        q_out_expected = complex(13, 4)
+        q_out_actual = free_space.act(q_in)
+        self.assertEquals(q_out_actual, q_out_expected)
+
+class TestThinLens(unittest.TestCase):
+    pass
+
+
+class TestOpticalPath(unittest.TestCase):
+    def test_propagate(self):
+        A = np.array([[0, 1], [2, 3]])
+        B = np.array([[4, 5], [6, 7]])
+        C = np.array([[8, 9], [8, 7]])
+        q_in = complex(3, 5)
+        # Vypočteno ručně
+        expected = complex(1_803_075/1_557_725, -320/1_557_725)
+
+        A_el = ABCDElement(A)
+        B_el = ABCDElement(B)
+        C_el = ABCDElement(C)
+
+        op = OpticalPath(A_el, B_el, C_el)
+        
+        actual = op.propagate(q_in)
+        self.assertAlmostEqual(expected, actual, places=3)
+
+    def test_init_number(self):
+        d1 = 5
+        d2 = 7
+        q_in = complex(3, 5)
+        expected = q_in + d1 +d2
+        op = OpticalPath(d1, d2)
+        actual = op.propagate(q_in)
+        self.assertEquals(actual, expected)
+
+    def test_init_number_ABCDElement(self):
+        d = 5
+        A = np.array([[1, 2], [3, 4]])
+        A_el = ABCDElement(A)
+
+        q_in = complex(3, 5)
+        expected = complex(355/1009, -10/1009)
+        op = OpticalPath(d, A_el)
+        actual = op.propagate(q_in)
+        self.assertAlmostEqual(expected, actual, places=3)
+        
+        
+
+        
