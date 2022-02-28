76fc0b2b5dbec80a310460d5cd0ab1ffc03db3e0
David <david.tomecek1@seznam.cz>
2022-02-28 14:04:04 +0100

    Updated intended audiance in setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Developers',      
+    'Intended Audience :: Science/Research',      
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.9',
