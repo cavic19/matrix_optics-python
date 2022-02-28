56d4297c79aaafc44a44778bb892adc0bd2b457a
David <david.tomecek1@seznam.cz>
2022-02-28 08:30:39 +0100

    Added more ABCD features

--- matrixopt/ABCDformalism.py
+++ matrixopt/ABCDformalism.py
@@ -4,7 +4,10 @@ from functools import reduce
 from matrixopt.beams import GaussianBeam
 
 class ABCDElement:
-    length = 0
+    @property
+    def length(self) -> float:
+        return 0
+
     def __init__(self, *args) -> None:
         """Accepts A, B, C, D matrix elements or a matrix itself"""
         if len(args) == 4:
@@ -33,17 +36,95 @@ class ABCDElement:
         return nom / denom
 
 
-class FreeSpace(ABCDElement):
-    def __init__(self, d: float, refr_index: float = 1) -> None:
-        self.length = d
-        self.n = refr_index
-        super().__init__(1, d / refr_index, 0, 1)
-  
+class PropagationInMedia(ABCDElement):
+    @property
+    def length(self) -> float:
+        return self._d
+
+    @property
+    def n(self) -> float:
+        return self._n
+
+    def __init__(self, d, n) -> None:
+        self._d = d
+        self._n = n
+        super().__init__(1, d / n, 0, 1)
+
+class FreeSpace(PropagationInMedia):
+    def __init__(self, d: float) -> None:
+        super().__init__(d, 1)
 
 class ThinLens(ABCDElement):
+    @property
+    def f(self):
+        return self._f
+
     def __init__(self, f: float) -> None:
-        self.f = f
+        self._f = f
         super().__init__(1, 0, -1/f, 1)
+
+
+class RefractionOnSphericalBoundary(ABCDElement):
+    @property
+    def n1(self):
+        return self._n1
+
+    @property
+    def n2(self):
+        return self._n2
+
+    @property
+    def R(self):
+        return self._R
+
+    def __init__(self, n1, n2, R) -> None:
+        """
+        Args:
+            n1 (float): Refractive index of the material the ray is propagating from
+            n2 (float): Refractive index of the material the ray is propagating to
+            R (float): Curviture of the boundary that is positive for convex boundary and negative for concave boundary.
+        """
+        self._n1 = n1
+        self._n2 = n2
+        self._R = R
+        super().__init__(self.__build_matrix())
+
+    def __build_matrix(self) -> np.ndarray:
+        return np.array([
+            [1,                                             0],
+            [-1*(self.n2 - self.n1) / (self.n2 * self.R),   self.n1 / self.n2]
+        ])
+
+
+class ThickLense(ABCDElement):
+    @property
+    def length(self) -> float:
+        return self._d
+
+    def __init__(self, R1, n, R2, d) -> None:
+        """ It is assumed, that the refractive index of free space is 1
+
+        Args:
+            R1 (float, positive): Curviture of the first face of the lense
+            n (float): Refractive index of the lense
+            R2 (float, positive): Curviture of the second face of the lense
+            d (float): Thickness of the lense
+        """
+        self._n = n
+        self._R1 = R1
+        self._R2 = R2
+        self._d = d
+
+        m = self.__build_matrix()
+        super().__init__(m)
+
+    def __build_matrix(self):
+        first_boundary = RefractionOnSphericalBoundary(1, self._n, self._R1).matrix
+        media = PropagationInMedia(self._d, self._n).matrix
+        second_boundary = RefractionOnSphericalBoundary(self._n, 1, -self._R2).matrix
+        return second_boundary.dot(media.dot(first_boundary))
+
+
 
 
 class OpticalPath:
